e2bc511c78ff1e4221311c036fadd57fa2389bed
Bianca <66906291+Biafcampos@users.noreply.github.com>
2020-07-17 11:29:17 -0300

    database

--- server/src/database/seeds/create_items.ts
+++ server/src/database/seeds/create_items.ts
@@ -3,7 +3,7 @@ import Knex from 'knex';
 
 export async function seed(knex: Knex) {
   await knex('items').insert([
-       {title: 'Lãmpadas', image: 'lãmpadas.svg' },
+       {title: 'Lãmpadas', image: 'lampadas.svg' },
        {title: 'Pilhas e baterias', image: 'baterias.svg' },
        {title: 'Papéis e Papelão', image: 'papeis-papelao.svg' },
        {title: 'Resíduos Eletrónicos', image: 'eletronicos.svg' },
